5a69815e950d571b70aebe4f5c07f90bc0066a2c
cwbishop <chriswbishop@gmail.com>
2013-12-13 11:30:24 -0800

    Linespec and pop_gaverager

---
+++ gab/tasks/gab_task_erplab_pop_gaverager.m
@@ -0,0 +1,18 @@
+function results=gab_task_erplab_pop_gaverager(args)
+%% DESCRIPTION:
+%
+%   Simple wrapper for ERPLAB's pop_gaverager function.
+%
+% INPUT:
+%
+%   args.
+%       ERPF:   cell array, ERP file names
+%       
+% OUTPUT:
+%
+%   results:    'done'
+%
+% Christopher W. Bishop
+%   University of Washington 
+%   12/13
+
